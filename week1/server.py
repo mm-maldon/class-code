7b0c977865aa98a132fec0aaeb5ad981a58896cc
Massimo Di Pierro <massimo.dipierro@gmail.com>
2023-04-03 21:01:54 -0700

    improved and more compact code

--- week1/server.py
+++ week1/server.py
@@ -6,67 +6,59 @@ def is_safe(filename):
     """make sure only serve files from inside the static folder"""
     return os.path.normpath(filename) == filename and filename.startswith("static/")
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def response_ok(http_body, mime_type):
+    http_headers = [
+        "HTTP/1.1 200 OK",
+        "Content-Type: %s; charset=utf-8" % mime_type,
+        "Content-Length: %s" % len(http_body),
+        "Content-Language: en-US",
+        "Date: Thu, 06 Dec 2018 17:37:18 GMT",
+        "Server: my-very-own-little-server",
+        ""
+    ]
+    return "\r\n".join(http_headers).encode() + b"\r\n" + http_body
 
-sock.bind(("0.0.0.0", int(sys.argv[1])))
+def response_404():
+    return b"HTTP/1.1 404 FILE NOT FOUND\r\n\r\n"
 
-sock.listen(1)
+def response_500():
+    return b"HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n"
 
-while True:
-    (conn, address) = sock.accept()
-    print(address)
-    request = conn.recv(10000).decode()
-    lines = request.split('\r\n')
-    a,filename,c = lines[0].split()
-    if filename == '/':
-        filename = "/index.html"
-    filename = "static" + filename    
-    if os.path.exists(filename) and is_safe(filename): 
-        if filename.endswith(".txt") or filename.endswith(".html"):
-            with open(filename) as stream:
-                http_body = stream.read()
-            http_headers = [
-                "HTTP/1.1 200 OK",
-                "Content-Type: text/html; charset=utf-8",
-                "Content-Length: %s" % len(http_body),
-                "Content-Language: en-US",
-                "Date: Thu, 06 Dec 2018 17:37:18 GMT",
-                "Server: my-very-own-little-server",
-                ""
-            ]
-            data = "\r\n".join(http_headers) + "\r\n" + http_body
-            conn.send(data.encode())
-            conn.close()
-        elif filename.endswith(".jpeg"):
+MIME_TYPES = {
+    "jpeg": "image/jpeg",
+    "png": "image/png",
+    "mp3": "audio/mp3",
+    "mov": "movie/mpeg4",
+}
+
+def main(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    sock.bind(("0.0.0.0", port))
+
+    sock.listen(1)
+
+    while True:
+        (conn, address) = sock.accept()
+        print(address)
+        request = conn.recv(10000).decode()
+        lines = request.split('\r\n')
+        a,filename,c = lines[0].split()
+        if filename == '/':
+            filename = "/index.html"
+        filename = "static" + filename    
+        extension = filename.split(".")[-1]
+        if not os.path.exists(filename) or not is_safe(filename): 
+            conn.send(response_404())
+        else:
+            extension = filename.split(".")[-1]
             with open(filename, "rb") as stream:
                 http_body = stream.read()
-
-            http_headers = [
-                "HTTP/1.1 200 OK",
-                "Content-Type: image/jpeg; charset=utf-8",
-                "Content-Length: %s" % len(http_body),
-                "Content-Language: en-US",
-                "Date: Thu, 06 Dec 2018 17:37:18 GMT",
-                "Server: my-very-own-little-server",
-                ""
-            ]
-            data = "\r\n".join(http_headers).encode() + b"\r\n" + http_body
-            conn.send(data)
-            conn.close()
-        else:
-            http_headers = [
-                "HTTP/1.1 500 INTERNAL SERVER ERROR",
-                ""
-                "Cannot handle this type of data"
-            ]
-            conn.send("\r\n".join(http_headers).encode())
-            conn.close()
-
-    else:
-        http_headers = [
-            "HTTP/1.1 404 NOT FOUND",
-            ""
-            "This file does not exist"
-        ]
-        conn.send("\r\n".join(http_headers).encode())
+            if extension in MIME_TYPES:
+                conn.send(response_ok(http_body, MIME_TYPES[extension]))
+            else:
+                conn.send(response_ok(http_body, "text/html"))
         conn.close()
+
+if __name__ == "__main__":
+    main(port=int(sys.argv[1]))
